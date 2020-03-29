6e6b0720454f24ab3e5665f1179a924131db6d42
kasraprime <ksr.darvish@gmail.com>
2020-03-29 13:17:23 -0400

    support loading mnist data in two fromats, binary and float

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import torch
 import torchvision
@@ -6,13 +7,17 @@ from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 import tqdm
 
-def mnist_loader():    
+def mnist_loader(data_type):    
     """Setup MNIST data loaders."""
     
     # Load datasets.
-    train_dataset = MNIST('data/pytorch_data/MNIST', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
-    test_dataset = MNIST('data/pytorch_data/MNIST', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
-    
+    if data_type == 'float':
+        train_dataset = MNIST('data/pytorch_data/MNIST', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+        test_dataset = MNIST('data/pytorch_data/MNIST', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    elif data_type == 'binary':
+        train_dataset = MNIST('data/pytorch_data/MNIST', train=True, download=True, transform=transforms.Compose([transforms.ToTensor(), lambda x:x>0, lambda x: x.float()]))
+        test_dataset = MNIST('data/pytorch_data/MNIST', train=False, download=True, transform=transforms.Compose([transforms.ToTensor(), lambda x:x>0, lambda x: x.float()]))
+
     kwargs = {'num_workers': 8, 'pin_memory': True, 'batch_size': 32, 'shuffle': False} 
     train_loader = DataLoader(train_dataset, **kwargs)
     test_loader = DataLoader(test_dataset, **kwargs)
@@ -23,3 +28,10 @@ train_loader, test_loader = mnist_loader()
 
 for epoch in tqdm(range(10)):
     for data in train_loader:
+        break
+
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_type', default='float', type=str)
+    mnist_loader(args.data_type)
