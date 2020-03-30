63e027b8f9c4d24e3db9906af7a11af5f8bc75cd
kasraprime <ksr.darvish@gmail.com>
2020-03-30 19:56:58 -0400

    batch size added as an argument

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 import tqdm
 import matplotlib.pyplot as plt
 
-def mnist_loader(data_type):    
+def mnist_loader(data_type,batch_size):    
     """Setup MNIST data loaders."""
     
     # Load datasets.
@@ -19,19 +19,18 @@ def mnist_loader(data_type):
         train_dataset = MNIST('data/pytorch_data/MNIST', train=True, download=True, transform=transforms.Compose([transforms.ToTensor(), lambda x:x>0, lambda x: x.float()]))
         test_dataset = MNIST('data/pytorch_data/MNIST', train=False, download=True, transform=transforms.Compose([transforms.ToTensor(), lambda x:x>0, lambda x: x.float()]))
 
-    kwargs = {'num_workers': 8, 'pin_memory': True, 'batch_size': 32, 'shuffle': False} 
+    kwargs = {'num_workers': 8, 'pin_memory': True, 'batch_size': batch_size, 'shuffle': False} 
     train_loader = DataLoader(train_dataset, **kwargs)
     test_loader = DataLoader(test_dataset, **kwargs)
     
     return train_loader, test_loader
 
-def train(data_type,epoch_num):
+def train(data_type,epoch_num,batch_size):
 
-    train_loader, test_loader = mnist_loader(data_type)
+    train_loader, test_loader = mnist_loader(data_type,batch_size)
 
-    for epoch in tqdm(range(epoch_num)):
-        for data in train_loader:            
-            break
+    
+
 
 
 
@@ -42,5 +41,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_type', default='float', type=str)
     parser.add_argument('--epoch_num', default=10, type=int)    
+    parser.add_argument('--batch_size', default=1, type=int)    
     args = parser.parse_args()
-    train(args.data_type,args.epoch_num)
+    train(args.data_type,args.epoch_num,args.batch_size)
