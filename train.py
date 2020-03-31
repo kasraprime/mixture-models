80d7366d32ff0d10ea4e0fa547e292b36ba54a3a
Kasra Darvish <ksr.darvish@gmail.com>
2020-03-31 01:36:13 -0400

    Initilizing pi in simplex K-1

--- train.py
+++ train.py
@@ -25,22 +25,58 @@ def mnist_loader(data_type,batch_size):
     
     return train_loader, test_loader
 
-def train(data_type,epoch_num,batch_size):
 
+def Simplex(K):    
+    X=[]
+    X.append(0)
+    for i in range(K-1):
+        X.append(np.random.uniform(0,1))
+    X.append(1)
+    X.sort()
+    Z=[]
+    for i in range(K):
+        Z.append(X[i+1] - X[i])
+
+    return Z
+
+
+
+def ComputePosterior(data_i,component_k):    
+        current_posterior=
+        for d in range(len(data_i)):
+            # data_i[d] represents x_{i,d}
+            for k in range(K_mixture):
+
+
+
+
+def train(data_type,epoch_num,batch_size,K_mixture):
+    """pi is a vector of length K, theta is of shape K*J, and in my case K is 10 and J=D=784"""
     train_loader, test_loader = mnist_loader(data_type,batch_size)
 
+    # initializing pi in simplex(K-1)
+    pi=Simplex(K_mixture)
     
+    for epoch in tqdm(range(epoch_num)):
+        for i, data in enumerate(train_loader):
+            # data[0] is the batch_size*1*28*28 matrix and data[1] is the label
+            # removing dimensions of size 1
+            data_temp=torch.squeeze(data[0])
+            # convert the shape of tensor from 28*28 to 784
+            data_flat=data_temp.view(-1)         
+            current_posterior=ComputePosterior(data_flat,component_k,pi)
+            current_posterior*
+            #update theta_{k,d}
+            theta_kd=
 
 
-
-
-
-
+        #update pi_{k}
 
 if __name__ == '__main__':    
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_type', default='float', type=str)
-    parser.add_argument('--epoch_num', default=10, type=int)    
-    parser.add_argument('--batch_size', default=1, type=int)    
+    parser.add_argument('--epoch_num', default=10, type=int)
+    parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--K_mixture', default=10, type=int)
     args = parser.parse_args()
-    train(args.data_type,args.epoch_num,args.batch_size)
+    train(args.data_type,args.epoch_num,args.batch_size,args.K_mixture)
